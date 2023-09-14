38222d81e2c0fb61c19fad348719176db897bcce
Sukhithaaithal <108125369+Sukhithaaithal@users.noreply.github.com>
2023-09-14 16:33:50 +0530

    Add files via upload

---
+++ fifo_scoreboard.sv
@@ -0,0 +1,49 @@
+class fifo_scoreboard extends uvm_scoreboard;
+  uvm_analysis_imp#(fifo_sequence_item, fifo_scoreboard) item_got_export;
+  `uvm_component_utils(fifo_scoreboard)
+  
+  function new(string name = "fifo_scoreboard", uvm_component parent);
+    super.new(name, parent);
+    item_got_export = new("item_got_export", this);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+  endfunction
+  
+  bit [127:0] queue[$];
+   
+  function void write(input fifo_sequence_item item_got);
+    bit [127:0] examdata;
+    if(item_got.i_wren == 'b1)begin
+      if(queue.size() < 1023) begin
+      queue.push_back(item_got.i_wrdata);
+      end
+        else
+          begin
+          $display("---------FIFO IS FULL------------");
+      end
+      `uvm_info("write Data", $sformatf("write: %0d read: %0d wrdata: %0d full: %0d",item_got.i_wren, item_got.i_rden,item_got.i_wrdata, item_got.o_full), UVM_LOW);
+    end
+    
+     if (item_got.i_rden == 'b1)begin
+      if(queue.size() >= 'd1)begin
+        examdata = queue.pop_front();
+      end
+       else
+         begin
+           $display("-------FIFO IS EMPTY---------");
+         end
+       `uvm_info("Read Data", $sformatf("examdata: %0d rddata: %0d empty: %0d", examdata, item_got.o_rddata, item_got.o_empty), UVM_LOW);
+        if(examdata == item_got.o_rddata)begin
+          $display("-------- Pass! --------");
+        end
+        else begin
+          $display("-------Fail!-------");
+        end
+     end
+  
+  endfunction
+ 
+endclass
+        
---
+++ fifo_sequence.sv
@@ -0,0 +1,57 @@
+ class fifo_sequence extends uvm_sequence#(fifo_sequence_item);
+
+  `uvm_object_utils(fifo_sequence)
+
+  function new(string name = "fifo_sequence");
+   super.new(name);
+  endfunction 
+
+   virtual task body();
+    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Write REQs ********"), UVM_LOW)
+     repeat(20) begin
+      req = fifo_sequence_item::type_id::create("req");
+      start_item(req);
+     assert(req.randomize() with {i_wren == 1;});
+      finish_item(req);
+    end
+
+    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Read REQs ********"), UVM_LOW)
+     repeat(20) begin
+      req = fifo_sequence_item::type_id::create("req");
+      start_item(req);
+     assert(req.randomize() with {i_rden == 1;});
+      finish_item(req);
+    end
+
+    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Random REQs ********"), UVM_LOW)
+     repeat(20) begin
+      req = fifo_sequence_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize());
+      finish_item(req);
+    end
+
+//     `uvm_info(get_type_name(), $sformatf("********* Generate 10 reads and writes *********"), UVM_LOW)
+//      repeat(20) begin
+//      req = fifo_sequence_item::type_id::create("req");
+//      start_item(req);
+//       assert(req.randomize() with {{i_wren,i_rden} == 2'b11;});
+//      finish_item(req);
+//     end
+
+//     `uvm_info(get_type_name(), $sformatf("********* Generate 10 no reads and writes *********"), UVM_LOW)
+//      repeat(20) begin
+//      req = fifo_sequence_item::type_id::create("req");
+//      start_item(req);
+//       assert(req.randomize() with {{i_wren,i_rden} == 2'b00;});
+//      finish_item(req);
+//     end
+   endtask
+ endclass
+  
+  
+     
+    
+     
+
+    
